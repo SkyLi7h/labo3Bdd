2f8596fc20aeda063da271771f7ef79b7fdbd6e9
SkyLi7h <skyli7h@gmail.com>
2016-09-29 16:20:52 +0200

    001

---
+++ labo3Bdd/CompanyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labo3Bdd
+{
+    class CompanyContext : DbContext
+    {
+
+        public DbSet<Customer> Customer { get; set; }
+        public CompanyContext():base(@"Data Source=(localdb)\MSSQLLocalDb; Initial Catalog=labo3Db")
+        {
+
+        }
+    }
+}
---
+++ labo3Bdd/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labo3Bdd
+{
+    class Customer
+    {
+        private double AccountBalance;
+        private string Adresseline1;
+        private string Adresseline2;
+        private string City;
+        private string Country;
+        private string EMail;
+        private long Id;
+        private string Name;
+        private string PostCode;
+        private string Remark;
+    }
+}
